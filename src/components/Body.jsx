7278f3ecc5bf9aab238a75bb8849467db892a577
belemaaa <graceitamunoala@gmail.com>
2023-08-28 11:07:21 +0000

    body

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import climate from '../assets/climate.jpg'
+import breeze from '../assets/breeze.jpg'
 import Header from './Header'
 
 const Body = () => {
@@ -7,11 +8,24 @@ const Body = () => {
     <div>
         <Header/>
         <div className='bg-white h-screen w-screen'>
-            <div className='md:py-10 md:px-32'>
-                <div className='bg-white md:rounded-lg h-[800px] md:h-[600px] shadow-xl'>
-                    <div className='flex flex-row'>
+            <div className='py-10 md:py-10 lg:px-32'>
+                <div className='bg-white md:rounded-lg h-[800px] md:h-[600px] shadow-sm'>
+                    <div className='flex flex-col md:flex-row items-center'>
                         <div>
-                            <img src={climate} className='md:ml-16 md:w-[450px] md:h-[530px] md:my-8 md:rounded-lg'/>
+                            <img src={climate} className='mx-4 md:ml-0 lg:ml-16 w-96 md:w-[450px] lg:w-[550px] md:h-[530px] md:my-8 rounded-lg'/>
+                        </div>
+                        <div className='flex flex-col text-center md:ml-16 lg:ml-16'>
+                            {/* <img src={breeze} className='w-8 h-8'/> */}
+                            <p className='text-xl font-light md:text-3xl mt-10 md:-mt-16 tracking-tighter'>
+                                Enter a city to retrieve the current weather data
+                            </p>
+                            <form method='POST'>
+                                <input 
+                                typeof='text'
+                                placeholder='e.g Lagos'
+                                className='border border-black w-80 mt-10 h-10 text-center placeholder:text-black placeholder:font-light'
+                                />
+                            </form>
                         </div>
                     </div>
                     
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className='bg-white py-5 drop-shadow-lg'>
-        <p className='text-3xl font-bold py-2 text-center md:text-start md:ml-24 text-slate-700 tracking-wide'>
+    <div className='bg-white py-5 drop-shadow-lg w-screen'>
+        <h1 className='text-xl md:text-3xl font-bold py-2 text-center text-slate-700 tracking-wide'>
             WeatherNOW.
-        </p>
+        </h1>
     </div>
   )
 }
