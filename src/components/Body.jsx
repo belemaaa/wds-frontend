492937bf4cecfb20a4999d3c0d95501b4ce33c51
belemaaa <graceitamunoala@gmail.com>
2023-08-28 09:21:00 +0000

    setup

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className='bg-gradient-to-b from-slate-500 to-slate-700 h-screen w-screen'>
+        <div className='mt-16'>
+            <div className='bg-black rounded-lg  h-[430px] text-white'>
+                hey
+            </div>
+        </div>
+        
+    </div>
   )
 }
 
