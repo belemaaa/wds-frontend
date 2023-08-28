df6e87f7acbdd5b7cbf360be7a91a49cb23211f9
belemaaa <graceitamunoala@gmail.com>
2023-08-28 10:11:03 +0000

    components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import climate from '../assets/climate.jpg'
+import Header from './Header'
 
 const Body = () => {
   return (
-    <div className='bg-gradient-to-b from-slate-500 to-slate-700 h-screen w-screen'>
-        <div className='mt-16'>
-            <div className='bg-black rounded-lg  h-[430px] text-white'>
-                hey
-            </div>
+    <div>
+        <Header/>
+        <div className='bg-white h-screen w-screen'>
+            <div className='md:py-10 md:px-32'>
+                <div className='bg-white md:rounded-lg h-[800px] md:h-[600px] shadow-xl'>
+                    <div className='flex flex-row'>
+                        <div>
+                            <img src={climate} className='md:ml-16 md:w-[450px] md:h-[530px] md:my-8 md:rounded-lg'/>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>         
         </div>
-        
     </div>
+   
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-white py-5 drop-shadow-lg'>
+        <p className='text-3xl font-bold py-2 text-center md:text-start md:ml-24 text-slate-700 tracking-wide'>
+            WeatherNOW.
+        </p>
+    </div>
+  )
+}
+
+export default Header
