16da48dc6f54b53faa250a59c50b647b9b5bde2c
belemaaa <graceitamunoala@gmail.com>
2023-08-28 12:32:15 +0000

    api response

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -3,11 +3,12 @@ import climate from '../assets/climate.jpg'
 import breeze from '../assets/breeze.jpg'
 import {BiSolidArrowFromLeft} from 'react-icons/bi'
 import Header from './Header'
-import axios from 'axios'
+
 
 const Body = () => {
     const [city, setCity] = useState('')
     const [showData, setShowData] =  useState(false)
+    const [data, setData] = useState([])
 
     const fetchWeatherData = async (e) => {
         e.preventDefault()
@@ -22,9 +23,14 @@ const Body = () => {
                     city: city
                 }),
             })
+            console.log(response)
+            const responseData = await response.json();
+
             if (response.status === 200){
                 setShowData(true)
                 console.log('success')
+                setData(responseData.weatherdata);
+                console.log(responseData.weatherdata)
             }
             else{
                 console.log('an error occurred')
@@ -41,23 +47,33 @@ const Body = () => {
                 <div className='bg-white md:rounded-lg h-[800px] md:h-[600px] shadow-sm'>
                     <div className='flex flex-col md:flex-row items-center'>
                         <div>
-                            <img src={climate} className='mx-4 md:ml-0 lg:ml-16 w-96 md:w-[450px] lg:w-[550px] md:h-[530px] md:my-8 rounded-lg'/>
+                            <img src={climate} className='items-center justify-center md:ml-0 lg:ml-16 w-[380px] md:w-[450px] lg:w-[550px] md:h-[530px] md:my-8 rounded-lg'/>
                         </div>
                         <div className='flex flex-col text-center md:ml-16 lg:ml-16'>
-                            {/* <img src={breeze} className='w-8 h-8'/> */}
-                            <p className='text-xl font-semibold md:text-3xl mt-10 md:-mt-16 tracking-tighter'>
-                                Enter a city to retrieve the current weather data:      
-                            </p>
-                            <form method='POST' onSubmit={fetchWeatherData}>
-                                <input 
-                                typeof='text'
-                                value={city}
-                                onChange={(e) => setCity(e.target.value)}
-                                placeholder='e.g Lagos'
-                                className='border border-black w-80 mt-10 h-10 text-center placeholder:text-gray-400 placeholder:font-light
-                                rounded-sm'
-                                />
-                            </form>
+                            {showData ? (
+                                <div>
+                                    <p></p>
+                                </div>
+                            ):(
+                               <div>
+                                    <p className='text-xl font-semibold md:text-3xl mt-10 md:-mt-16 tracking-tighter'>
+                                        Enter a city to retrieve the current weather data:      
+                                    </p>
+                                    <form method='POST' onSubmit={fetchWeatherData}>
+                                        <input 
+                                        typeof='text'
+                                        value={city}
+                                        onChange={(e) => setCity(e.target.value)}
+                                        placeholder='e.g Lagos'
+                                        className='border border-black w-80 mt-10 h-10 text-center placeholder:text-gray-400 placeholder:font-light
+                                        rounded-sm'
+                                        />
+                                    </form>
+                                </div>
+                                
+                            )
+                            }
+                            
                         </div>
                     </div>
                     
